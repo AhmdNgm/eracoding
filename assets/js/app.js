d67808455c53b6e81ed13653216e1f285126ede2
Ahmed Negm <ahmdn4418@gmail.com>
2022-02-08 19:55:15 +0200

    test

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  navbar.classList.toggle("fixed-top", window.scrollY > 20);
+});
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 20000,
+  dots: true,
+  navText: [$(".prev"), $(".next")],
+  responsiveClass: true,
+  smartSpeed: 2000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+function smallNav() {
+  var element = document.getElementById("mobNav");
+  element.classList.toggle("mobile-nav");
+}
